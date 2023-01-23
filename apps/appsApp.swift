91633474f8ff855c10d9d0e8e742d672920ea030
Hyunsub Kim <embrapers263@gmail.com>
2023-01-23 21:26:44 +0900

    Initial Commit

---
+++ apps/appsApp.swift
@@ -0,0 +1,17 @@
+//
+//  appsApp.swift
+//  apps
+//
+//  Created by USER on 2023/01/23.
+//
+
+import SwiftUI
+
+@main
+struct appsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
